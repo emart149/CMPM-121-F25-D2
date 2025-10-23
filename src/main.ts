3f21649dba1f202e90464ed016eefb40d90ab6e5
emart149 <emart149@ucsc.edu>
2025-10-23 05:29:26 +0000

    added event listeners for when the clicking the mouse down, the mouse moving, and the mouse being released so that the x and y variables can be changed according to the mouse's location
    In addition, mouse down sets the isDrawing Boolean to True which allows drawLine to be called which essentially connects two points that are entered into the argument.
    
    While Mouseup event makes isDrawing bool false so that drawLine during mousemove event does not execute
    
    clear button implemented through element and eventlistener which basically makes the square fully green again
    
    (NOTE: Collaborated with Mahir Camci, mcamci@ucsc.edu, on his repository via liveShare)
    (Step 2 Complete)

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,18 @@ document.body.innerHTML = `
 `;
 
 const titleElement = document.createElement("h1");
-titleElement.innerHTML = "D2 Sticker Assignment";
+titleElement.innerHTML = "CMPM 170 Homework";
 
 document.body.appendChild(titleElement);
 
 const canvas = document.createElement("canvas")!;
-
 canvas.id = "canvasVar";
 const ctx = canvas.getContext("2d")!;
+
+const clearButton = document.createElement("button") as HTMLButtonElement;
+clearButton.id = "clearButton";
+clearButton.innerHTML = "Clear";
+document.body.appendChild(clearButton);
 
 canvas.width = 256;
 canvas.height = 256;
@@ -22,3 +26,54 @@ document.body.appendChild(canvas);
 
 ctx.fillStyle = "green";
 ctx.fillRect(0, 0, 256, 256);
+
+let isDrawing: boolean = false;
+let x = 0;
+let y = 0;
+
+clearButton.addEventListener("click", () => {
+  ctx.fillStyle = "green";
+  ctx.fillRect(0, 0, 256, 256);
+});
+
+canvas.addEventListener("mousedown", (e) => {
+  x = e.offsetX;
+  y = e.offsetY;
+  console.log("x: " + x);
+  console.log("y: " + y);
+
+  isDrawing = true;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (isDrawing) {
+    drawLine(ctx, x, y, e.offsetX, e.offsetY);
+    x = e.offsetX;
+    y = e.offsetY;
+  }
+});
+
+globalThis.addEventListener("mouseup", (e) => {
+  if (isDrawing) {
+    drawLine(ctx, x, y, e.offsetX, e.offsetY);
+    x = 0;
+    y = 0;
+    isDrawing = false;
+  }
+});
+
+function drawLine(
+  ctx: CanvasRenderingContext2D,
+  x1: number,
+  y1: number,
+  x2: number,
+  y2: number,
+) {
+  ctx.beginPath();
+  ctx.strokeStyle = "black";
+  ctx.lineWidth = 1;
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.stroke();
+  ctx.closePath();
+}
