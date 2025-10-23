087a981516ec6550fb1db8df59e95d5c42e604fd
emart149 <emart149@ucsc.edu>
2025-10-23 05:25:12 +0000

    added h1 element that states assignment name
    cteated ctx variables so that we can utilized the 2D context on canvas
    
    Set width, height, and position of canvas
    
    created a green square with rounded borders in the canvas utilizing css file also
    
    (NOTE: Collaborated with Mahir Camci, mcamci@ucsc.edu, on his repository via liveShare)
    (Step 1 Complete)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,24 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
 document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
 `;
+
+const titleElement = document.createElement("h1");
+titleElement.innerHTML = "D2 Sticker Assignment";
+
+document.body.appendChild(titleElement);
+
+const canvas = document.createElement("canvas")!;
+
+canvas.id = "canvasVar";
+const ctx = canvas.getContext("2d")!;
+
+canvas.width = 256;
+canvas.height = 256;
+canvas.style.position = "absolute";
+canvas.style.left = "50px";
+
+document.body.appendChild(canvas);
+
+ctx.fillStyle = "green";
+ctx.fillRect(0, 0, 256, 256);
