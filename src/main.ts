ebbbf06d47ade936869b0d8e235b08bc408ef932
emart149 <emart149@ucsc.edu>
2025-10-23 06:24:34 +0000

    created point interface so that each line that is created is esentially an array of points that contain x and y properties
    in order to be able to draw separate lines that do not automatically connect, we made anothher array, lineArr, which stores multiple arrays, with individual line arrays starting with mouse down and ending with mouse up.
    
    In addition, when mouse is down the line is immediately pushed to the lineArr and points are added to the pointArr while still in the lineArr
    
    For some reason, Mahir's codeSpace with redraw function not checking if thisLine[i] & thisLine[i+1] are not undefined in redraw but in order for me to commit I needed to verify they are defined
    
    (NOTE: Collaborated with Mahir Camci, mcamci@ucsc.edu, on his repository via liveShare)
    (Step 3 Complete)

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
+// deno-lint-ignore-file
+// deno-lint-ignore-file prefer-const
+
 import "./style.css";
 
 document.body.innerHTML = `
 `;
 
 const titleElement = document.createElement("h1");
-titleElement.innerHTML = "CMPM 170 Homework";
+titleElement.innerHTML = "D2 Sticker Assignment";
 
 document.body.appendChild(titleElement);
 
@@ -31,36 +34,13 @@ let isDrawing: boolean = false;
 let x = 0;
 let y = 0;
 
-clearButton.addEventListener("click", () => {
-  ctx.fillStyle = "green";
-  ctx.fillRect(0, 0, 256, 256);
-});
+interface point {
+  x: number;
+  y: number;
+}
 
-canvas.addEventListener("mousedown", (e) => {
-  x = e.offsetX;
-  y = e.offsetY;
-  console.log("x: " + x);
-  console.log("y: " + y);
-
-  isDrawing = true;
-});
-
-canvas.addEventListener("mousemove", (e) => {
-  if (isDrawing) {
-    drawLine(ctx, x, y, e.offsetX, e.offsetY);
-    x = e.offsetX;
-    y = e.offsetY;
-  }
-});
-
-globalThis.addEventListener("mouseup", (e) => {
-  if (isDrawing) {
-    drawLine(ctx, x, y, e.offsetX, e.offsetY);
-    x = 0;
-    y = 0;
-    isDrawing = false;
-  }
-});
+let lineArr: point[][] = [];
+let pointArr: point[] = [];
 
 function drawLine(
   ctx: CanvasRenderingContext2D,
@@ -77,3 +57,98 @@ function drawLine(
   ctx.stroke();
   ctx.closePath();
 }
+
+function redraw() {
+  ctx.fillStyle = "green";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  for (let thisLine of lineArr) {
+    for (let i = 0; i < thisLine.length - 1; i++) {
+      let curPoint = thisLine[i];
+      let nextPoint = thisLine[i + 1];
+
+      if (curPoint && nextPoint) {
+        drawLine(
+          ctx,
+          curPoint.x,
+          curPoint.y,
+          nextPoint.x,
+          nextPoint.y,
+        );
+      }
+    }
+  }
+}
+
+const bus = new EventTarget();
+
+function notify(name: string) {
+  bus.dispatchEvent(new Event(name));
+}
+
+bus.addEventListener("drawing-changed", redraw);
+
+clearButton.addEventListener("click", () => {
+  lineArr = [];
+  pointArr = [];
+  ctx.fillStyle = "green";
+  ctx.fillRect(0, 0, 256, 256);
+});
+
+canvas.addEventListener("mousedown", (e) => {
+  x = e.offsetX;
+  y = e.offsetY;
+
+  let newPoint: point = { x: x, y: y };
+  pointArr = [];
+  pointArr.push(newPoint);
+  lineArr.push(pointArr);
+  notify("drawing-changed");
+  isDrawing = true;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (isDrawing) {
+    x = e.offsetX;
+    y = e.offsetY;
+
+    let newPoint: point = { x: x, y: y };
+
+    pointArr.push(newPoint);
+
+    notify("drawing-changed");
+  }
+});
+
+/*function printArr(sampleArr: point[]) {
+  for (let pointElement of sampleArr) {
+    console.log("X: " + pointElement.x + " Y: " + pointElement.y);
+  }
+} */
+
+globalThis.addEventListener("mouseup", () => {
+  if (isDrawing) {
+    notify("drawing-changed");
+    x = 0;
+    y = 0;
+    pointArr = [];
+    isDrawing = false;
+  }
+});
+
+//let testPoint1: point = { x: 29, y: 60 };
+//let testPoint2: point = { x: 127, y: 201 };
+//let testPoint3: point = { x: 207, y: 60 };
+
+/*pointArr.push(testPoint1);
+pointArr.push(testPoint2);
+pointArr.push(testPoint3);
+
+printArr(pointArr);
+
+redraw(pointArr);*/
+
+//how to do step 4:
+//undo just deletes last element of line array, redo references buffer that stored the deleted
+//line
+//(Note: make buffer big enough to store muliple lines)
+// maybe makeinterface for each line stored
